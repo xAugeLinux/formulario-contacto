f965d326fada9dc4021bedd81fbe3bf16b37a9d4
Jose Alonso <jose.alonso.26.02.03@gmail.com>
2026-01-27 12:34:55 -0600

    refactor: corrección de logica

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: node({
-        mode: 'standalone',
-  }),
+  // GitHub Pages configuration
+  site: 'https://xaugelinux.github.io/formulario-contacto',
+  base: '/formulario-contacto',
+  output: 'static',
+
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'always',
+    assetsInlineLimit: 10240, // 10KB to inline achievements.css (7.5KB)
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      assetsInlineLimit: 10240,
+    }
+  },
+
+  integrations: [react()]
 });
