acfbd64ed057f89be497659eac1381c4cf26b6f3
Jose Alonso <jose.alonso.26.02.03@gmail.com>
2026-01-27 12:42:31 -0600

    refactor: corrección de errores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
@@ -12,16 +11,17 @@ export default defineConfig({
   output: 'static',
 
   compressHTML: true,
+  
   build: {
     inlineStylesheets: 'always',
-    assetsInlineLimit: 10240, // 10KB to inline achievements.css (7.5KB)
   },
+  
   vite: {
     plugins: [tailwindcss()],
     build: {
-      assetsInlineLimit: 10240,
+      assetsInlineLimit: 10240
     }
   },
-
+  
   integrations: [react()]
 });
