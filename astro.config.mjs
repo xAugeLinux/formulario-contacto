c00c9566ecdeaaa80b187d8763af4a8d93324fa5
Jose Alonso <jose.alonso.26.02.03@gmail.com>
2026-01-28 20:48:12 -0600

    refactor: cambios de configuración.
    feat: Iconos de redes sociales.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+const isGithub = process.env.DEPLOY_TARGET === 'github';
 
 export default defineConfig({
-  site: 'https://xaugelinux.github.io',
-  base: '/formulario-contacto/',
+  site: isGithub ? 'https://xaugelinux.github.io' : undefined,
+  base: isGithub ? '/formulario-contacto/' : '/',
   output: 'static',
 
   compressHTML: true,
@@ -23,3 +24,4 @@ export default defineConfig({
 
   integrations: [react()],
 });
+
